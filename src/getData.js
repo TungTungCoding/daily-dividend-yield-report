0fc19eceff6785787bb1876f81a5d0d72f3d544e
tungtungCoding <ttungcoding@gmail.com>
2021-03-15 22:23:55 +0900

    marketbeat crawling

--- src/getData.js
+++ src/getData.js
@@ -1,7 +1,34 @@
-
-
 import axios from "axios"
 import cheerio from "cheerio"
+
+export const getDividendYiled = async (symbol) => {
+  return await axios.get(`https://www.marketbeat.com/stocks/NASDAQ/${symbol}/dividend/`)
+    .then(html => {
+      const $ = cheerio.load(html.data)
+      const dividendYiled = $("div.price-data-col:nth-child(3) div.price-data:nth-child(2) strong").text()
+
+      if (dividendYiled === "N/A") {
+        return "currently no dividends"
+      } else if (dividendYiled === "") {
+        return "no data exist"
+      } else {
+        return dividendYiled
+      }
+    }).catch(() => {
+      return "Error occurred during web crawling process"
+    })
+}
+
+const getData = async (stockSymbolList) => {
+  let dividendYiledList = []
+  for (let symbol of stockSymbolList) {
+    const dividendYiled = await getDividendYiled(symbol)
+    dividendYiledList.push({ symbol, dividendYiled })
+  }
+  return dividendYiledList
+}
+
+export default getData
 
 /*
 // 아래의 크롤링 코드는 값을 읽어오지 못합니다.
@@ -15,3 +42,7 @@ export const getDividendYiled = async (symbol) => {
     })
 }
 */
+
+
+
+
